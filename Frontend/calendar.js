fbe85cedb4394533efd3d4a4dfcc3375d62131ff
komalsaini06 <k3654464@gmail.com>
2026-08-10 15:11:43 +0530

    Add files via upload

---
+++ Frontend/calendar.js
@@ -0,0 +1,120 @@
+// Crop Calendar JavaScript
+
+
+async function getCalendar(){
+
+
+    const crop = document.getElementById("crop").value;
+
+
+
+    if(crop === ""){
+
+        alert("Please select crop");
+
+        return;
+
+    }
+
+
+
+
+    const data = {
+
+        crop: crop
+
+    };
+
+
+
+
+    try{
+
+
+        const response = await fetch(
+
+            "http://127.0.0.1:8000/calendar/recommend",
+
+            {
+
+                method:"POST",
+
+                headers:{
+
+                    "Content-Type":"application/json"
+
+                },
+
+
+                body:JSON.stringify(data)
+
+            }
+
+        );
+
+
+
+        const result = await response.json();
+
+
+
+
+
+        document.getElementById("result").innerHTML = `
+
+
+
+        <p>
+        🌱 Crop :
+        <b>${result.crop}</b>
+        </p>
+
+
+
+        <p>
+        🌾 Sowing Time :
+        ${result.sowing}
+        </p>
+
+
+
+        <p>
+        🌿 Fertilizer Schedule :
+        ${result.fertilizer}
+        </p>
+
+
+
+        <p>
+        💧 Irrigation :
+        ${result.irrigation}
+        </p>
+
+
+
+        <p>
+        🚜 Harvesting :
+        ${result.harvesting}
+        </p>
+
+
+
+        `;
+
+
+
+    }
+
+
+    catch(error){
+
+
+        console.log(error);
+
+        alert("Unable to load crop calendar");
+
+
+    }
+
+
+}
