f21a2e30e51ab4f3c291addae2de648fdfd84952
komalsaini06 <k3654464@gmail.com>
2026-08-10 15:33:45 +0530

    Add files via upload

---
+++ Frontend/script.js
@@ -0,0 +1,35 @@
+alert("script loaded");
+document.getElementById("predictionForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const data = {
+        farmer_name: document.getElementById("farmer_name").value,
+        nitrogen: Number(document.getElementById("nitrogen").value),
+        phosphorus: Number(document.getElementById("phosphorus").value),
+        potassium: Number(document.getElementById("potassium").value),
+        temperature: Number(document.getElementById("temperature").value),
+        humidity: Number(document.getElementById("humidity").value),
+        ph: Number(document.getElementById("ph").value),
+        rainfall: Number(document.getElementById("rainfall").value)
+    };
+
+    try {
+        const response = await fetch("http://127.0.0.1:8000/prediction", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        document.getElementById("result").innerHTML =
+            `<h3>Recommended Crop: ${result.predicted_crop}</h3>`;
+
+    } catch (error) {
+        console.error(error);
+        document.getElementById("result").innerHTML =
+            "<h3>Something went wrong!</h3>";
+    }
+});
