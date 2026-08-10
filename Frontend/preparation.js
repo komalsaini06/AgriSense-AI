5ce8a8bf0d23295ed753079e92aeee0a611d9500
komalsaini06 <k3654464@gmail.com>
2026-08-10 15:26:35 +0530

    Add files via upload

---
+++ Frontend/preparation.js
@@ -0,0 +1,128 @@
+// Land Preparation Recommendation
+
+
+async function getPreparation(){
+
+
+    const crop = document.getElementById("crop").value;
+
+    const soil = document.getElementById("soil").value;
+
+
+
+    if(crop === "" || soil === ""){
+
+        alert("Please select crop and soil type");
+
+        return;
+
+    }
+
+
+
+    const data = {
+
+
+        crop: crop,
+
+
+        soil_type: soil
+
+
+    };
+
+
+
+
+    try{
+
+
+        const response = await fetch(
+
+            "http://127.0.0.1:8000/preparation/recommend",
+
+            {
+
+                method:"POST",
+
+                headers:{
+
+                    "Content-Type":"application/json"
+
+                },
+
+
+                body:JSON.stringify(data)
+
+            }
+
+        );
+
+
+
+        const result = await response.json();
+
+
+
+
+        let steps = "";
+
+
+
+        result.steps.forEach(function(step, index){
+
+
+            steps += `<p>${index + 1}. ${step}</p>`;
+
+
+        });
+
+
+
+
+
+
+        document.getElementById("result").innerHTML = `
+
+
+
+        <p>
+        🚜 Crop :
+        <b>${result.crop}</b>
+        </p>
+
+
+
+        <h3>Preparation Steps:</h3>
+
+        ${steps}
+
+
+
+        <p>
+        🌱 Soil Improvement :
+        ${result.soil_improvement}
+        </p>
+
+
+
+        `;
+
+
+
+    }
+
+
+    catch(error){
+
+
+        console.log(error);
+
+        alert("Unable to get preparation plan");
+
+
+    }
+
+
+
+}
