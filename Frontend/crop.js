48cb5f57177674f99d5e4e469297ae547dee09b5
komalsaini06 <k3654464@gmail.com>
2026-08-15 17:20:58 +0530

    Change land fetching API to production URL
    Updated land fetching API endpoint to a production URL.

--- Frontend/crop.js
+++ Frontend/crop.js
@@ -39,9 +39,9 @@ async function loadLands() {
 
     try {
 
-        const response = await fetch(
-            "http://127.0.0.1:8000/crop/lands"
-        );
+        const response = await fetch("https://agrisense-ai-fua5.onrender.com/prediction")
+            
+        ;
 
         if (!response.ok) {
 
